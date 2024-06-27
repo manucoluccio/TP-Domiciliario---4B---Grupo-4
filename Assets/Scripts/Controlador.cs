922820f211796add98f7e57344fb20a356225815
FacuBraslaa <facundobraslavsky@gmail.com>
2024-06-26 22:45:22 -0300

    Declaracion de variables

--- Assets/Scripts/Controlador.cs
+++ Assets/Scripts/Controlador.cs
@@ -1,9 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controlador : MonoBehaviour
 {
+    public GameObject[] Objetos;
+    Dictionary<GameObject, int> precio = new Dictionary<GameObject, int>();
+    GameObject ProductoRandom;
+    public Text txt_preciosSuma;
+    public Text txt1;
+    public Text txt2;
+    public Text txt3;
+    public Text txt_btnVolverAJugar;
+    public Text txt_notificacion;
+    public GameObject panelMal_Bien;
+    public GameObject panelSeleccion;
+    int rprecioRandom;
+    int SumaDePrecio;
+    int Productos;
+    int precioSuma;
+    int precio1;
+    int precio2;
+    int precio3;
     // Start is called before the first frame update
     void Start()
     {
